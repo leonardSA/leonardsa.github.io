cbc28b5693846289d9a59ab5c3028c5711a1916d
leonardSA <leonard.stephenauguste@gmail.com>
2019-08-24 23:09:53 +0200

    scripts: fix git clone

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,7 +13,7 @@ git init
 git config --global user.email 'travis@travis-ci.org'
 git config --global user.name 'Travis'
 # Fetch repo
-git remote add deploy $GITHUB_TOKEN@github.com:leonardSA/leonardsa.github.io.git
+git remote add deploy git@github.com:leonardSA/leonardsa.github.io.git
 git fetch --all
 # Copy gh-pages and push
 git checkout gh-pages
