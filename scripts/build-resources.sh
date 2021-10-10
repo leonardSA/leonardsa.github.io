2e4c2ab5c187c977089cf2e761f88501be696eb3
leonardSA <leonard.stephenauguste@gmail.com>
2021-10-10 20:07:44 +0200

    script,build-resources: add to build .uml and .gv files
    .uml: plantuml
    .gv: graphviz

---
+++ scripts/build-resources.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# File              : build-resources.sh
+# Author            : leonardSA <leonard.stephenauguste@gmail.com>
+# Date              : 10.10.2021
+# Last Modified Date: 10.10.2021
+# Last Modified By  : leonardSA <leonard.stephenauguste@gmail.com>
+
+ROOT_DIR=..
+PAGES_DIR=_pages # dir containing pages with resources
+PLANTUML_JAR=./scripts/lib/plantuml.1.2021.12.jar # relative to ROOT_DIR
+
+# ARG will be substituted
+DOT_CMD="dot -Tjpg ARG -Ojpg"
+PLANT_CMD="java -Djava.awt.headless=true -jar $PLANTUML_JAR -Tpng ARG"
+
+# by pair
+FILETYPE=(gv uml)
+COMMANDS=("$DOT_CMD" "$PLANT_CMD")
+
+cd $ROOT_DIR
+# for every filetype
+for i in "${!FILETYPE[@]}"; do  
+    # search directory for files with the filetype and compile them
+    for file in $(find $PAGES_DIR -name *.${FILETYPE[i]}); do
+        ${COMMANDS[i]/ARG/"$file"} 2> $file.log  # substitute and execute
+        if [ $? -ne 0  ]; then
+            1>&2 printf "\033[1;31m[KO] %s : %s\n\033[0m" $0 $file
+        else
+            printf "\033[1;32m[OK] %s : %s\n\033[0m" $0 $file
+            rm $file.log
+        fi
+    done
+done
